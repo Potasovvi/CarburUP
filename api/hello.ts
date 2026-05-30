543f66a5a75023e8b236e05f935fd44407c804d4
Andrea Rovarotti <rovarotti@gmail.com>
2026-05-30 21:35:13 +0200

    debug: test JS-based serverless function instead of TS

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-export default async function handler(_req: any, res: any) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ ok: true, message: 'Hello from Vercel' }))
-}
